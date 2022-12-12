3195edb8f8baddd70270cc97af6cfd89fe45bd71
Polo Kong <polo1314502@gmail.com>
2022-12-12 22:43:10 +0800

    fou to for

--- test.js
+++ test.js
@@ -1,11 +1,8 @@
 console.log("hi, I am Polo");
 console.log("Hi,I am Fei");
-console.log("HIHI");
-console.log("hihihih");
-console.log("fuck you");
-console.log("hihihih");
 
-console.log("thank you fou your help");
+console.log("thank you for your help");
+
 // git add .
 // git commit -m "???"
 // git push
